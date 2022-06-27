6b2afdd8a2c5ed311efb1b1ebbd0b516f01961a8
Aleksandr_UB <bogdanov777000@mail.ru>
2022-06-27 16:30:19 +0300

    4 -sort и пузырьковая сортировка users.sort((a,b)=> a.name.localeCompare(b.name)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 /*
+/!*
 // 1 занятие JS: Копирование объектов в Реакт:
 const user = {
     name: "Bob",
@@ -25,7 +26,7 @@ const copyStudents = students.map(student => ({...student}))
 // 2 занятие JS: : map filter()...........
 ....................................................................................................
 ....................................................................................................
-*/
+*!/
 
 // 3 lesson:
 // события всплавают target, currenTarget(тот элемент который вызывает функцию обработчик иногда таргет и каренттаргет совпадают но потом их пути расходятся) - событие на котором произошел клик и всплытие не распространяется дальше на элементы
@@ -49,4 +50,51 @@ const onClickHandler = (e) => {
 // sm.addEventListener('click', () => {alert('kkk')})
 
 sm.addEventListener('click', onClickHandler)
-// sm.removeEventListener('click', onClickHandler)
+// sm.removeEventListener('click', onClickHandler)*/
+
+
+// 4 lesson: sort() - сортировка алгоритм очень сложный:
+
+/*
+// очередность по юникоду: сначала цифры потом заглавные английские и тд см. таблицу юникода:
+const names = ['Alex', 'Bob', 'Donald', 'Vlad', '007', 'alex', 'юрий']
+
+console.log(names.sort())
+
+const numbers = [100, 99, 665, 1000, 2]
+
+// return  > 0 => перестановка
+// return  <= 0 => перестановки не будет
+
+const compFn = (a, b) => { // по возрастанию сравниваются попарно числа
+    if (a > b) {
+        return 10 // вернем лубое число большее нуля
+    } else {
+        return -10
+    }
+}
+
+console.log([...numbers].sort((a, b) => a - b)) // sort "мутабельный" метод
+
+// localCompare() - метод сравнивает и сортирует строки удобно использовать когда сортируем объекты
+
+// users.sort((a,b)=> a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1)
+// users.sort((a,b)=> a.name.localeCompare(b.name)*/
+
+// bubble sort: пузырьковая сортировка - цикл в цикле потому что иначе мы просто самое большое число продвинем в конецБ а тут все числа: сложность O(n2 - тяжелый алгоритм)
+const nums = [13, 45, 67, 12, 99, 87] // ПО ВОЗРАСТАНИЮ
+for (let i = 0; i < nums.length - 1; i++) {
+    let isSorted = true // если отсортирован то выйдем из цикла
+    for (let j = 0; j < nums.length - 1 - j; i++) {
+        if (nums[i] > nums[i + 1]) {
+            isSorted = false;
+            /*     let temp = nums[i]
+                 nums[i] = nums[i + 1]
+                 nums[i + 1] = temp   */
+// деструктуризация:
+            [nums[i + 1], [nums[i]]] = [nums[i], nums[i + 1]]
+        }
+    }
+    if (isSorted) break
+}
+
